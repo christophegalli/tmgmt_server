6812a9dae7ff4cd5a0f33eb46fc3511047cce786
Christophe Galli <christophe@galligalli.ch>
2016-06-15 17:20:31 +0200

    first

---
+++ src/Controller/TMGMTServerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\tmgmt_server\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class TMGMTServerController.
+ *
+ * @package Drupal\tmgmt_server\Controller
+ */
+class TMGMTServerController extends ControllerBase {
+
+  /**
+   * Addtranslation.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function addRemoteTranslation(Request $Request) {
+
+    $response['data'] = 'Antwort: ' . $Request->get('galli');
+    return  new JsonResponse($response) ;
+  }
+
+}
